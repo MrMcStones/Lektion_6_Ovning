42e93b28c88a9fd3250e99f76833ab4bba781e6f
MrMcStones <rasmus.ke.nilsson@gmail.com>
2023-10-04 21:32:59 +0200

    Uppgift 1

--- src/com/rasmus/demo/Main.java
+++ src/com/rasmus/demo/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            System.out.println("Inside TRY");
-            System.out.println(2/0);
-        } catch (Exception error) {
-            error.printStackTrace();
-            System.out.println("Catch started");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            System.out.println("Line to put 'breakpoint'");
         }
     }
 }
